3ee6d91a10a6086a1800ee3674a83d2b7efe144f
kradib <radibkar32@gmail.com>
2025-03-01 23:42:03 +0530

    Add get all documents functionality with dummy data.

--- src/api/document.ts
+++ src/api/document.ts
@@ -1,6 +1,7 @@
 import { ApiRoutes } from "../Constants";
 import { IRequest, RequestMethod } from "../interface/IRequest";
 import sendRequest from "./request";
+import docData from "./dummyDocs.json";
 
 export const uploadDocument = async (file: any, docName: string) => {
   const documentData = new FormData();
@@ -24,4 +25,9 @@ export const uploadDocument = async (file: any, docName: string) => {
     } with error: ${JSON.stringify(response)}`
   );
   return { success: false, data: response.data.data?.metadata?.errors[0] };
+};
+
+export const getDocuments = (pageNumber: number, pageSize: number) => {
+  console.log(pageNumber, pageSize);
+  return { success: true, data: { data: { documents: docData, totalElements: docData.length } } };
 };
--- src/pages/Documents.tsx
+++ src/pages/Documents.tsx
@@ -1,12 +1,32 @@
 import { useTheme } from "@mui/material";
+import Box from "@mui/material/Box";
+import Pagination from "@mui/material/Pagination";
+import TablePagination from "@mui/material/TablePagination";
+import TableBody from "@mui/material/TableBody";
+import Paper from "@mui/material/Paper";
+import TableCell from "@mui/material/TableCell";
+import Table from "@mui/material/Table";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import UploadFileIcon from "@mui/icons-material/UploadFile";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import UploadDocumentModal from "../components/UploadDocumentModal";
 import Toast from "../components/Toast";
+import { getDocuments } from "../api/document";
+import LoadingComponent from "../components/LoadingComponent";
+import DownloadIcon from "@mui/icons-material/Download";
 
 const Documents = () => {
     const theme = useTheme();
+
+    const style = {
+        backgroundColor: theme.palette.primary.light,
+        color: "black",
+        fontWeight: "bold",
+    };
 
     const [showUploadModal, setShowUploadModal] = useState(false);
 
@@ -15,6 +35,25 @@ const Documents = () => {
     const [toastSeverity, setToastSeverity] = useState<"success" | "error">(
         "success"
     );
+
+    const [isLoading, setLoading] = useState(false);
+
+    const [documents, setDocuments] = useState([]);
+
+    const [pageNumber, setPageNumber] = useState(0);
+    const [pageSize, setPageSize] = useState(5);
+    const [totalCount, setTotalCount] = useState(0);
+
+    const getAllDocuments = () => {
+        setLoading(true);
+        const response = getDocuments(pageNumber + 1, pageSize);
+        setLoading(false);
+
+        if (response.success) {
+            setDocuments(response.data?.data.documents);
+            setTotalCount(response.data?.data.totalElements);
+        }
+    };
 
     const handleCloseModal = () => {
         setShowUploadModal(false);
@@ -31,8 +70,79 @@ const Documents = () => {
         setShowUploadModal(false);
     };
 
+    useEffect(() => {
+        getAllDocuments();
+    }, [pageNumber, pageSize]);
+
+    const noDocumentsComponent = () => {
+        if (documents.length == 0 && !isLoading) {
+            return (
+                <>
+                    <Typography variant="h6" sx={{ textAlign: "center" }}>
+                        You have no documents uploaded yet
+                    </Typography>
+                </>
+            );
+        }
+    };
+
+    const documentListComponent = () => {
+        const handleChangePage = (_event: any, page: number) => {
+            setPageNumber(page);
+        };
+
+        return (
+            <>
+                <LoadingComponent isLoading={isLoading} />
+
+                <TableContainer component={Paper}>
+                    <Table sx={{ xs: 300, md: 600 }} aria-label="customized table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell sx={style}>Document Name</TableCell>
+                                <TableCell sx={style}>Date Uploaded</TableCell>
+                                <TableCell sx={style}>Download</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {documents.map((doc, index) => (
+                                <TableRow key={index}>
+                                    <TableCell>{doc.name}</TableCell>
+                                    <TableCell>{doc.dateUploaded}</TableCell>
+                                    <TableCell>
+                                        <IconButton
+                                            sx={{ color: theme.palette.primary.main }}
+                                            onClick={() => {
+                                                console.log(doc.identifier);
+                                            }}
+                                        >
+                                            <DownloadIcon />
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10]}
+                    component="div"
+                    count={totalCount}
+                    rowsPerPage={pageSize}
+                    page={pageNumber}
+                    onPageChange={(_event, page) => setPageNumber(page)}
+                    onRowsPerPageChange={(event) =>
+                        setPageSize(parseInt(event.target.value))
+                    }
+                />
+            </>
+        );
+    };
+
     return (
         <>
+            {noDocumentsComponent()}
+            {documentListComponent()}
             <IconButton
                 sx={{
                     backgroundColor: theme.palette.primary.main,
