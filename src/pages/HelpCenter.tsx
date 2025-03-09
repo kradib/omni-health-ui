d438287ff59337d369e31c1e0b35944412c9e516
kradib <radibkar32@gmail.com>
2025-03-10 00:12:58 +0530

    Update help centre component

--- src/pages/HelpCenter.tsx
+++ src/pages/HelpCenter.tsx
@@ -7,8 +7,8 @@ const HelpCenter = () => {
             <Stack spacing={2}>
                 <Typography variant="h1">Contact Us</Typography>
                 <Typography variant="body1">For more information or support reach out to:</Typography>
-                <Typography variant="body1"><b>Phone Number:{` `}</b>+12-1234567890</Typography>
-                <Typography variant="body1"><b>Email:{` `}</b>support@omnihealth.com</Typography>
+                <Typography variant="body1"><b>Phone Number:{` `}</b>+971-508811557</Typography>
+                <Typography variant="body1"><b>Email:{` `}</b>omnihealthapp@gmail.com</Typography>
             </Stack>
         </>
     )
